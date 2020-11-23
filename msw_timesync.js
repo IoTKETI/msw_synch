f3d7c646a7a95cbe0e9b18aca0191503158789f7
IoTKETI <iot.keti@gmail.com>
2020-11-23 18:21:37 +0900

    Update msw_timesync.js

--- msw_timesync.js
+++ msw_timesync.js
@@ -119,7 +119,7 @@ function runLib(obj_lib) {
 
         run_lib.stderr.on('data', function(data) {
             console.log('stderr: ' + data);
-            setTimeout(init, 1000);
+//             setTimeout(init, 1000);
         });
 
         run_lib.on('exit', function(code) {
