7684bc1d10596fb6dd6d2a0a2ada44b570a8b056
IoTKETI <iot.keti@gmail.com>
2021-07-16 20:10:45 +0900

    - change command

--- msw_timesync.js
+++ msw_timesync.js
@@ -128,7 +128,7 @@ function runLib(obj_lib) {
             scripts_arr[0] = './' + config.directory_name + '/' + scripts_arr[0];
         }
 
-        let run_lib = spawn('sudo', ['python3', scripts_arr]);
+        let run_lib = spawn('sudo', ['python3', scripts_arr[0] + '.py', scripts_arr.slice(1)]);
 
         run_lib.stdout.on('data', function(data) {
             console.log('stdout: ' + data);
