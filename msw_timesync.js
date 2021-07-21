ec7469f0db8dabcd1491b7231d7891616bba3782
IoTKETI <iot.keti@gmail.com>
2021-07-21 14:09:23 +0900

    - remove log

--- msw_timesync.js
+++ msw_timesync.js
@@ -242,7 +242,6 @@ function on_receive_from_lib(topic, str_message) {
 }
 
 function on_process_fc_data(topic, str_message) {
-    console.log('[' + topic + '] ' + str_message + '\n');
     let topic_arr = topic.split('/');
     try {
         fc[topic_arr[topic_arr.length-1]] = JSON.parse(str_message.toString());
